f4a4e89b854721e5bed3e33b0989ee4190722381
Andri Reveli <reveliandri@gmail.com>
2023-06-22 21:27:49 +0200

    Bump mandrel version
    - Add the latest mandrel version
    - Remove Java 11
    - Add Java 20

--- build.py
+++ build.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 SUPPORTED_FEDORA_VERSIONS: list[int] = [36, 37, 38]
-SUPPORTED_JAVA_VERSIONS: list[int] = [11, 17]
-MANDREL_VERSION: str = "21.3.6.0-Final"
+SUPPORTED_JAVA_VERSIONS: list[int] = [17, 20]
+MANDREL_VERSION: str = "23.0.0.0-Final"
 
 
 def main() -> None:
